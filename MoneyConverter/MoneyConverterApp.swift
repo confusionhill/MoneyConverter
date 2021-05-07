0514b3a40281918749b938e082ec6a7517eca754
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-05-07 20:24:35 +0700

    Initial Commit

---
+++ MoneyConverter/MoneyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyConverterApp.swift
+//  MoneyConverter
+//
+//  Created by Farhandika Zahrir Mufti guenia on 07/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MoneyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
